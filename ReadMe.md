ea870fc22d604860070a244b97f71a74e204b081
Isaac <mercado.isaac619@gmail.com>
2022-01-04 22:11:03 -0800

    Finished product

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# Password Generator
+
+```bash
+Created a password generator where the application enables employees to generate random passwords based on criteria that they’ve selected.
+```
+
+![image](./assets/image/Screenshot3.png)
+
+[Portfolio-Showcase](https://isaacmercado619.github.io/password-generator/)
--- script.js
+++ script.js
@@ -1,14 +1,65 @@
-// Assignment Code
+//Selecting elements to use
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
+//Data input for password function
+let numbers = "0123456789";
+let lowerCase = "abcdefghijklomnpqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLOMNPQRSTUVWXYZ";
+let specialChar = "!@#$%^&*";
+let selection =  [];
+let userPassword = [];
+
+//Function for generating password
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  let passwordLength = prompt('Please enter the length of characters you would like to generate, but the length of characters must be between 8 to 128.')
+  if(!passwordLength){
+    return;
+  }
 
-}
+  if(
+    (passwordLength <= 7 || passwordLength >= 129) ||
+    (!parseInt(passwordLength)) ){
+    alert('Please enter within the range.');
+    return writePassword();
+  }
+
+  let numbersSelected = confirm('Do you want numbers?');
+  if(numbersSelected){
+    selection.push(...numbers);
+  }
+
+  let specialCharSelected = confirm('Do you want special characters?')
+  if(specialCharSelected){
+    selection.push(...specialChar);
+  }
+
+  let lowerCaseSelected = confirm('Do you want lower case letters?')
+  if(lowerCaseSelected){
+    selection.push(...lowerCase);
+  }
+
+  let upperCaseSelected = confirm('Do you want upper case letters?')
+  if(upperCaseSelected){
+    selection.push(...upperCase);
+  }
+
+  if(!numbersSelected, !specialCharSelected, !lowerCaseSelected, !upperCaseSelected){
+    alert('Please select at least one of the choices!')
+    return writePassword();
+  }
+
+  console.log(selection);
+
+//Formula for generating random characters.
+  for (var i = 0; i < passwordLength; i++) {
+    userPassword.push(selection[Math.floor(Math.random() * selection.length)]);
+    console.log(userPassword.join('+'));
+  };
+//Display value of password on the element selected.
+    passwordText.value = userPassword.join('');
+  };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
